2d3a8cb6ebe42da77dde0be9bf97e98b63d45b29
sohailB <bhattisohail20@gmail.com>
2023-11-21 12:54:38 +0500

    code updated with logs

--- pages/api/change-cron.js
+++ pages/api/change-cron.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+    console.log("req", req)
     if (req.method === 'POST') {
         let { timing } = req.body
         console.log("timing", timing)
