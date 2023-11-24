f087e4b26eab810d4d9ef31425cc1a4741f04db0
sohail20 <49647481+sohail20@users.noreply.github.com>
2023-11-24 14:55:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Helloasdasdss")
+const { SiteClient } = require('datocms-client');
+const { changeCronTiming } = require('./pages/api/cron');
+const client = new SiteClient('7ff35345302d3d594080cd643c9486');
+
+(async function getRecordById(recordId) {
+    try {
+        const record = await client.items.find(recordId);
+
+        if (record) {
+            console.log('Retrieved record:', record);
+            changeCronTiming(`*/${record.time} * * * *`)
+            return record;
+        } else {
+            console.log('Record not found');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error retrieving record:', error);
+        return null;
+    }
+})("NhGzknBMRf-QeO1lgKWtBQ");
